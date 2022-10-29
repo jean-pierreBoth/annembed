7959bf2f798da9ea3696848a3b52c5070bc1195a
Jean-Pierre Both <jeanpierre.both@gmail.com>
2022-10-29 18:16:17 +0200

    modifs quantiles on hubness

--- src/fromhnsw/hubness.rs
+++ src/fromhnsw/hubness.rs
@@ -110,7 +110,7 @@ impl <'a,F> Hubness<'a,F>
         if nb_out_histo > 0 {
             println!("number of too large values : {}, maximum value : {}", nb_out_histo, max_value);
         }
-        let quantiles = vec![0.1, 0.25, 0.5, 0.6, 0.7, 0.8, 0.9 , 0.99];
+        let quantiles = vec![0.1, 0.25, 0.5, 0.75, 0.9 , 0.99, 0.999];
         let thresholds = quantiles.iter().map(|f| histo.value_at_quantile(*f)).collect::<Vec<u64>>();
         //
         println!("quantiles : {:?}", quantiles);
